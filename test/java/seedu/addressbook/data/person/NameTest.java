254371256bb71b3ca7c73bdbe47c4042d4a9bcd4
Lim Jie <limjie1994@gmail.com>
2017-02-08 12:46:42 +0800

    Added null test

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -3,12 +3,21 @@ package seedu.addressbook.data.person;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.rules.TestName;
+
+import seedu.addressbook.data.exception.IllegalValueException;
 
 public class NameTest {
 
     @Test
     public void testIsSimilar() {
-        fail("Not yet implemented");
+        Name Testname = null;
+        try {
+            Testname = new Name("Lim Jie");
+        } catch (IllegalValueException ive) {
+            throw new RuntimeException("test name should be valid by definition");
+        }
+        assertFalse(Testname.isSimilar(null));
     }
 
 }
